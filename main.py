a3dfdd29f71b26b1dec7f8d86bec2f3ba237f2fe
costy04 <costin.costinu2000@gmail.com>
2025-07-07 16:17:52 +0300

    Added reader CSV function

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def main():
-    pass
+from reader_csv import read_and_parse_csv
+
+def main ():
+    data_grouped = read_and_parse_csv("logs.log")
+    print("Data grouped by PID:")
+    if data_grouped is not None:
+        for pid, group in data_grouped:
+            print(f"PID: {pid}")
+            print(group)
 
 if __name__ == "__main__":
     main()
--- reader_csv.py
+++ reader_csv.py
@@ -1,2 +1,21 @@
-def read_and_parse_csv():
-    pass
+import pandas as pd
+import os
+
+COLUMN_NAMES = ['Timestamp', 'Description', 'Status', 'PID']
+
+def read_and_parse_csv(input_path):
+# This function reads a CSV file and parses it into a DataFrame, grouping by PID.
+
+    if not os.path.exists(input_path):
+        print(f"Error: File {input_path} doesn't exist.")
+        return None
+    
+    try:
+        data = pd.read_csv(input_path, header=None, names=COLUMN_NAMES)
+    except Exception as e:
+        print(f"Error reading CSV file: {e}")
+        return None
+    
+    data['Timestamp'] = pd.to_datetime(data['Timestamp'], format='%H:%M:%S')
+    grouped = data.groupby(["PID"])
+    return grouped
